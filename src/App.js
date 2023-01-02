0b07e7e2112f4f57f333a0b0c496c15f92d3fbc5
Luis Muñoz <l.munoz.m@outlook.com>
2023-01-02 12:28:13 -0300

    Add Routes
    - Add routes on App,js
    - Add react-router-dom package

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useEffect } from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import BikeList from './features/bikeList/BikeList';
 import './App.css';
 import store from './app/store';
@@ -10,9 +11,11 @@ function App() {
   });
 
   return (
-    <div className="App">
-      <BikeList />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<BikeList />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
