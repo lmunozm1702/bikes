346b766fac2f41b2c399bbb4d4e9968810d2777b
Luis Muñoz <l.munoz.m@outlook.com>
2023-01-02 16:22:37 -0300

    Add bike list render for Europe
    - Filters the results of the API by one unique continent, in this case Europe.
    
    - Render the bike providers, grouped by country.

--- src/features/bikeList/BikeList.js
+++ src/features/bikeList/BikeList.js
@@ -1,3 +1,36 @@
-const BikeList = () => <div>Bike kist!</div>;
+import { useSelector } from 'react-redux';
+
+const BikeList = () => {
+  const groupedBikeList = useSelector((state) => state.bikeList.countryBikeList);
+  const totalProviders = useSelector((state) => state.bikeList.totalProviders);
+
+  const countryHandleClick = (event) => {
+    event.preventDefault();
+    console.log(event);
+  };
+
+  return (
+    <>
+      <div>
+        <div>Europe</div>
+        <div>{totalProviders}</div>
+      </div>
+      <ul>
+        {groupedBikeList
+          && Object.keys(groupedBikeList).map((country) => (
+            <li key={country}>
+              <div role="button" id={country} onClick={countryHandleClick} onKeyDown={countryHandleClick} tabIndex={0}>
+                <div id={country}>
+                  {country}
+                </div>
+                <div id={country}>{groupedBikeList[country].length}</div>
+              </div>
+
+            </li>
+          ))}
+      </ul>
+    </>
+  );
+};
 
 export default BikeList;
--- src/features/bikeList/BikeListReducer.js
+++ src/features/bikeList/BikeListReducer.js
@@ -1,19 +1,34 @@
 const GET_BIKES = 'bikesList/getBikes';
 const GET_BIKES_FULFILLED = 'bikesList/getBikes/fulfilled';
+const _ = require('lodash');
+
+const EUROPE = ['BE', 'BG', 'CZ', 'DK', 'DE', 'EE', 'IE', 'EL', 'ES', 'FR', 'HR', 'IT', 'CY', 'LV', 'LT', 'LU', 'HU', 'MT', 'NL', 'AT', 'PL', 'PT', 'RO', 'SI', 'SK', 'FI', 'SE'];
+// const COUNTRIES = ['IT'];
 
 function getBikes(payload) {
-  console.log('payload', payload);
   return {
     type: GET_BIKES,
     payload,
   };
 }
 
-export default function reducer(state = [], action = {}) {
-  console.log(action.payload);
+export default function reducer(state = {}, action = {}) {
+  let bikesGrouped = {};
+  let newArray = [];
+
   switch (action.type) {
     case GET_BIKES_FULFILLED:
-      return { worldwideBikeList: [action.payload.networks] };
+      if (action.payload.networks) {
+        // eslint-disable-next-line max-len
+        newArray = action.payload.networks.filter((elem) => EUROPE.some((f) => f === elem.location.country));
+        bikesGrouped = _.groupBy(newArray, (bike) => bike.location.country);
+      }
+
+      return {
+        worldwideBikeList: [...action.payload.networks],
+        countryBikeList: { ...bikesGrouped },
+        totalProviders: newArray.length,
+      };
     default:
       return state;
   }
